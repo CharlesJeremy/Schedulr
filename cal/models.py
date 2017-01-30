8a3af7905690785e2cb6af0cdd935e364e920cb0
Rebecca Wang <rwang7@stanford.edu>
2017-01-29 21:56:18 -0800

    defined model for generic event and registered on admin page

--- cal/models.py
+++ cal/models.py
@@ -2,4 +2,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+from courses.models import Course
+from django.contrib.auth.models import User
+
+class Event(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length = 200)
+    description  = models.TextField()
+    location = models.CharField(max_length = 200)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
