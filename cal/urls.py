238fae1e4221aa657f304b84ca182e8e884e0963
Wen Zhang <zhangwen@cs.stanford.edu>
2017-02-06 20:57:38 -0800

    User add events; closes #16

--- cal/urls.py
+++ cal/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
         url(r'^$', views.index, name='index'),
         url(r'^get_event_feed$', views.get_event_feed, name='get_event_feed'),
         url(r'^add_course$', views.add_course, name='add_course'),
+        url(r'^add_event$', views.add_event, name='add_event'),
 ]
