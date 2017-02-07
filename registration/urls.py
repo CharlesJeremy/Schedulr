428976308ff456cffc97ad47a390c4e4634f8222
hcaseyal <hcaseyal@stanford.edu>
2017-02-06 17:04:30 -0800

    Registration page accessible. Logout button actually logs out

--- registration/urls.py
+++ registration/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 
+from .views import register
+
 urlpatterns = [
         url(r'^login/$', auth_views.login, name='login'),
         url(r'^logout/$', auth_views.logout, { 'next_page': '/' }, name='logout'),
+        url(r'^register/$', register, name='register'),
 ]
