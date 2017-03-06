b2f22f56728b71a36856067374a8177176ae92ba
Wen Zhang <zhangwen@cs.stanford.edu>
2017-03-06 14:05:03 +0000

    factored out refresh course logic

--- courses/admin.py
+++ courses/admin.py
@@ -41,45 +41,48 @@ class CourseAdmin(nested_admin.NestedModelAdminMixin, AdminViews):
 
     search_fields = ('subject', 'code')
 
+    @staticmethod
+    def refresh_courses():
+        Course.objects.all().delete()
+        Section.objects.all().delete()
+        Schedule.objects.all().delete()
+
+        # fresh_courses = []
+        # fresh_sections = []
+        # fresh_schedules = []
+        for c in extract_courses():
+            course = Course.objects.create(subject=c.subject, code=c.code, title=c.title)
+            # fresh_courses.append(course)
+            for s in c.sections:
+                section = Section.objects.create(course=course, class_id=s.class_id,
+                        term_year=s.term.year, term_quarter=Quarter.parse(s.term.quarter),
+                        section_number=s.section_number, component=s.component,
+                        instructors=s.instructors)
+                # fresh_sections.append(section)
+
+                sc = s.schedule
+                if sc:
+                    Schedule.objects.create(section=section, start_date=sc.start_date,
+                            end_date=sc.end_date,
+                            days_of_week=DaysOfWeek(sc.days_of_week),
+                            start_time=sc.start_time, end_time=sc.end_time,
+                            location=sc.location)
+                    # fresh_schedules.append(schedule)
+
+        # TODO(zhangwen): bulk doesn't work yet.
+        '''
+        Course.objects.bulk_create(fresh_courses)
+        Section.objects.bulk_create(fresh_sections)
+        Schedule.objects.bulk_create(fresh_schedules)
+        '''
+
     @transaction.atomic
     def refresh_course_schedules(self, request, *args, **kwargs):
         """
         Clears old course schedules and re-imports them from ExploreCourses.
         """
         if request.POST:
-            Course.objects.all().delete()
-            Section.objects.all().delete()
-            Schedule.objects.all().delete()
-
-            # fresh_courses = []
-            # fresh_sections = []
-            # fresh_schedules = []
-            for c in extract_courses():
-                course = Course.objects.create(subject=c.subject, code=c.code, title=c.title)
-                # fresh_courses.append(course)
-                for s in c.sections:
-                    section = Section.objects.create(course=course, class_id=s.class_id,
-                            term_year=s.term.year, term_quarter=Quarter.parse(s.term.quarter),
-                            section_number=s.section_number, component=s.component,
-                            instructors=s.instructors)
-                    # fresh_sections.append(section)
-
-                    sc = s.schedule
-                    if sc:
-                        Schedule.objects.create(section=section, start_date=sc.start_date,
-                                end_date=sc.end_date,
-                                days_of_week=DaysOfWeek(sc.days_of_week),
-                                start_time=sc.start_time, end_time=sc.end_time,
-                                location=sc.location)
-                        # fresh_schedules.append(schedule)
-
-            # TODO(zhangwen): bulk doesn't work yet.
-            '''
-            Course.objects.bulk_create(fresh_courses)
-            Section.objects.bulk_create(fresh_sections)
-            Schedule.objects.bulk_create(fresh_schedules)
-            '''
-
+            CourseAdmin.refresh_courses()
             messages.success(request, "Successfully refreshed course schedules")
             return redirect('admin:index')
         else:
